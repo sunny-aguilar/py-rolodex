83e6686ab0704962527ba7e617c93edcfc4bd46e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-27 19:12:07 -0700

    rolodex

--- rolodex.py
+++ rolodex.py
@@ -7,6 +7,7 @@ LOOK_UP = 1
 ADD = 2
 CHANGE = 3
 DELETE = 4
+QUIT = 5
 
 FILENAME = 'contacts.dat'
 
@@ -27,7 +28,45 @@ def main():
             delete(mycontacts)
 
 
-save_contatrs(mycontacts)
+save_contacts(mycontacts)
 
 
+def load_contacts():
+    try:
+        input_file = open(FILENAME, 'rb')
+        contact_dct = pickle.load(input_file)
+        input_file.close()
+    except IOError:
+        contact_dct = {}
+
+    return contact_dct
+
+def get_menu_choice():
+    print()
+    print('Menu')
+    print('-----------------------------')
+    print('1. Look up a contact')
+    print('2. Add a new contact')
+    print('3. Change an existing contact')
+    print('4. Delete a contact')
+    print('5. Quit the program')
+
+    choice = int(input('Enter your choice: '))
+    while choice < LOOK_UP or choice > QUIT:
+        choice = int(input('Enter a valid choice: '))
+
+    return choice
+
+
+def load_contacts():
+    input_file = open(FILENAME, 'rb')
+
+    contact_dct = pickle.load(input_file)
+
+    input_file.close()
+
+    except IOError:
+        contact_dct = {}
+
+    return contact_dct
 
